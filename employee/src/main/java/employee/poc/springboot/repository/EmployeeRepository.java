850e12f4bcbfaccac9b4766b02c736df7c55fd56
Mukul Anand <mukulanandchas@gmail.com>
2022-01-10 12:15:05 +0530

    Add files via upload

---
+++ employee/src/main/java/employee/poc/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package employee.poc.springboot.repository;
+
+import employee.poc.springboot.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, Long>{
+
+}
